06847454714e3500824d3bc53a964a78c0e51cd6
Babatunde Alfred Olorundajo <tundealfred.office@gmail.com>
2023-09-14 01:07:41 +0100

    Add confirm.JS file

---
+++ confirm.js
@@ -0,0 +1,8 @@
+let text = "Press a button!\nEither OK or Cancel.";
+if (confirm(text) == true) {
+  text = "You pressed OK!";
+} else {
+  text = "You canceled!";
+}
+document.getElementById("demo").innerHTML = text;
+document.write(text);
