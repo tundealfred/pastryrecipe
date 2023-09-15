c3afde531c04b83009e0c176cce469e5838e1277
Babatunde Alfred Olorundajo <tundealfred.office@gmail.com>
2023-09-15 11:50:26 +0100

    modified js & html, add image

--- app.js
+++ app.js
@@ -6,15 +6,25 @@ function makeReserv() {
   
   let compare = prompt("Do you want to make a reservation? (Yes/No)");
   
-  if (compare === "Yes") {
-    message = "Table 5 Available!";
-  } else if (compare === "No") {
-    message = "Look at our Yummy Pastry";
-  } else {
-    message = "My friend, Let us know when you're HUNGRY!";
-  }
+    if (compare === "Yes") {
+      message = "Table 5 Available!";
+    } else if (compare === "No") {
+      message = "Look at our Yummy Pastry";
+    } else {
+      message = "My friend, Let us know when you're HUNGRY!";
+    }
   
-  message = "Hello " + username + ". " + message;
+    message = "Hello " + username + ". " + message;
 
-  document.write(message);
+    document.write(message);
+}
+
+
+function getRating() {
+
+  let rating = prompt('Please rate me 1 - 5','5');
+
+  for (let i = 0; i < rating; i++) {
+    document.write("<img src='images/star.png'/>");
+  }
 }
